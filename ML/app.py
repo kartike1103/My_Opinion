532fe07d4667f486af97a123e7520677428dfc49
Subrat Behera <sb998001@gmail.com>
2024-07-06 23:41:20 +0530

    ml done

---
+++ ML/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+import joblib
+
+app = Flask(__name__)
+
+# Load the model and vectorizer
+model = joblib.load('hate_speech_model.pkl')
+vectorizer = joblib.load('count_vectorizer.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        data = vectorizer.transform([message]).toarray()
+        prediction = model.predict(data)
+        return render_template('result.html', prediction=prediction[0])
+
+if __name__ == '__main__':
+    app.run(debug=True)
